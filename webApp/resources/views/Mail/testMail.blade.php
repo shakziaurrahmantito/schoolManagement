1785de0441ec0cccb37a91bff12ddac0e8aa8477
Md. Ziaur Rahman <49427684+shakziaurrahmantito@users.noreply.github.com>
2022-07-19 07:29:31 +0600

    text update

--- webApp/resources/views/Mail/testMail.blade.php
+++ webApp/resources/views/Mail/testMail.blade.php
@@ -6,14 +6,14 @@
 		<title>Sender Mail</title>
 	</head>
 	<body>
-		<p>Welcome to mail</p>
+		<p>Dear Teachers</p>
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			Best wishes and congratulations to you all from our School Management System. We invite you to use and test this application.
+			<p>
+				Regard By <br>
+				Md. Ziaur Rahman
+			</p>
+
 		</p>
 	</body>
 </html>
